d5affc5926aaddb24ee0b054a250aabcc8e5fa69
FrancescoBenassi <francybenna77@gmail.com>
2024-11-25 19:24:24 +0100

    feat RegistroStudenti: added methods

--- src/RegistroStudenti.java
+++ src/RegistroStudenti.java
@@ -1,3 +1,17 @@
-public class RegistroStudenti {
+import java.util.ArrayList;
 
+public class RegistroStudenti {
+    private ArrayList<Studente> studentList = new ArrayList<>();
+
+    public RegistroStudenti(){
+
+    }
+
+    public void addStudent(Studente Student){
+        this.studentList.add(Student);
+    }
+
+    public ArrayList<Studente> getStudents(){
+        return studentList;
+    }
 }
